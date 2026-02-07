8aaa8924f3eb0a9dbfa8acf26de858c7713f6625
Disha-arch <guptadisha.1009@gmail.com>
2026-02-08 00:08:11 +0530

    updates in deleteNote controller

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -37,6 +37,13 @@ export const updateNote = async (req,res)=>{
     }
 }
 
-export const deleteNote = (req,res)=>{
-    res.status(201).json({message:"Note is deleted successfully !!"})
+export const deleteNote = async (req,res)=>{
+    try {
+        const deletedNote = await Note.findByIdAndDelete(req.params.id);
+        if(!deletedNote) return res.status(404).json({message:"Note not found !"});
+        res.status(200).json({message:"Note deleted successfully !! "});
+    } catch (error) {
+        console.error("Error in deleteNote controller" , error);
+        res.status(500).json({message:"Internal server error",error});
+    }
 }
