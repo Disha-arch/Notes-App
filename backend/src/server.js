bfb621fc524aeda2ad991cd89e428d1451e149eb
Disha-arch <guptadisha.1009@gmail.com>
2026-02-08 19:30:26 +0530

    ratelimiter

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoute.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json()); // to parse the incoming JSON data and access it in the req.body object.
+app.use(rateLimiter);
 
 //our simple custom middleware
 // app.use((req,res,next)=>{
