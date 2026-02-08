5dd601263ba9945618ae8961b96278ad5986da6b
Disha-arch <guptadisha.1009@gmail.com>
2026-02-08 19:38:04 +0530

    updates

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,17 +13,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // to parse the incoming JSON data and access it in the req.body object.
 app.use(rateLimiter);
 
-//our simple custom middleware
-// app.use((req,res,next)=>{
-//     console.log(`Req method is ${req.method} & req url is ${req.url}`);
-//     next();
-// })
-
-connectDB();
 
 app.use("/api/notes" , notesRoutes);
 
-
-app.listen(PORT , ()=>{
-    console.log("Server started at port :" , PORT);
+// this means that first database will be connected and then only server will listen
+connectDB().then(()=>{
+    app.listen(PORT , ()=>{
+        console.log("Server started at port :" , PORT);
+    })
 })
+
