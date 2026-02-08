11d693e9f1f49d51c6429b1bb761209a19d60b04
Disha-arch <guptadisha.1009@gmail.com>
2026-02-08 14:17:32 +0530

    middleware intro

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 5000;
 //middleware
 app.use(express.json()); // to parse the incoming JSON data and access it in the req.body object.
 
+//our simple custom middleware
+// app.use((req,res,next)=>{
+//     console.log(`Req method is ${req.method} & req url is ${req.url}`);
+//     next();
+// })
 
 connectDB();
 
