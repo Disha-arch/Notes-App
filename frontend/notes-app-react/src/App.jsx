97f4596c5a42ad2f765abdec3739e5a0820829c3
Disha-arch <guptadisha.1009@gmail.com>
2026-02-09 21:46:30 +0530

    tailwindcss update

--- frontend/notes-app-react/src/App.jsx
+++ frontend/notes-app-react/src/App.jsx
@@ -2,10 +2,12 @@ import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import NoteDetailPage from "./pages/NoteDetailPage";
 import { Route, Routes } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const App = () => {
     return(
         <div>
+            <button onClick={()=>toast.success("Congrats")}>click me !</button>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/create" element={<CreatePage/>} />
