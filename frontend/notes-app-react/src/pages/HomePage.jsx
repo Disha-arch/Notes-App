27790cc75be0dc4ef888a7b82c833c3dc4dd46ed
Disha-arch <guptadisha.1009@gmail.com>
2026-02-23 16:47:56 +0530

    updates

--- frontend/notes-app-react/src/pages/HomePage.jsx
+++ frontend/notes-app-react/src/pages/HomePage.jsx
@@ -3,7 +3,9 @@ import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
 
 const HomePage = () => {
-    const[isRateLimited , setIsRateLimited] = useState(false);
+    const[isRateLimited , setIsRateLimited] = useState(true);
+    const[notes , setNotes]= useState([]);
+    const[loading , setLoading] = useState(true)
     return(
         <div className="min-h-screen">
             <Navbar />
