16b3de31d78d77de43f01d0ad7e361e8846c322f
Disha-arch <guptadisha.1009@gmail.com>
2026-02-27 22:04:03 +0530

    updates

--- frontend/notes-app-react/src/pages/HomePage.jsx
+++ frontend/notes-app-react/src/pages/HomePage.jsx
@@ -40,7 +40,11 @@ const HomePage = () => {
                 {loading && <div className="text-center text-primary py-10">Loading notes......</div>}
                 {notes.length > 0 && !isRateLimited && (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-
+                        {notes.map(note => (
+                            <div>
+                                {note.title} | {note.content}
+                            </div>
+                        ))}
                     </div>
                 )}
             </div>
