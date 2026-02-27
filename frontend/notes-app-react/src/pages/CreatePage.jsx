14e4233e5ade05d9f7c9b6cab0238265ae63bd1a
Disha-arch <guptadisha.1009@gmail.com>
2026-02-28 01:27:17 +0530

    updates

--- frontend/notes-app-react/src/pages/CreatePage.jsx
+++ frontend/notes-app-react/src/pages/CreatePage.jsx
@@ -7,6 +7,9 @@ const CreatePage = () => {
     const[title , setTitle] = useState("");
     const[content , setContent] = useState("");
     const[loading , setLoading] = useState(false);
+
+    const handleSubmit = () => {}
+
     return(
         <div className="min-h-screen bg-base-200">
             <div className="container mx-auto px-4 py-8">
@@ -15,6 +18,18 @@ const CreatePage = () => {
                         <ArrowLeftIcon className="size-5" />
                         Back to Notes
                     </Link>
+                    <div className="card bg-base-100">
+                        <div className="card-body">
+                            <h2 className="card-title text-2xl mb-4">Create New Note</h2>
+                            <form onSubmit={handleSubmit}>
+                                <div className="form-control mb-4">
+                                    <label className="label">
+                                        <span className="label-text">Title</span>
+                                    </label>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
