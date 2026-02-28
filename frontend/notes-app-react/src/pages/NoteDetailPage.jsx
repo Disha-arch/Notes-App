552cb5b05927aa9c6191b92ad93e08888b1afb49
Disha-arch <guptadisha.1009@gmail.com>
2026-02-28 19:28:21 +0530

    updates

--- frontend/notes-app-react/src/pages/NoteDetailPage.jsx
+++ frontend/notes-app-react/src/pages/NoteDetailPage.jsx
@@ -1,4 +1,34 @@
+import { useState ,  useEffect } from "react";
+import {useNavigate , useParams} from "react-router-dom"
+import api from "../lib/axios"
+import toast from "react-hot-toast"
+
 const NoteDetailPage = () => {
+    const[note,setNote]=useState(null);
+    const[loading , setLoading] = useState(true);
+    const[saving , setSaving] = useState(false);
+
+    const navigate = useNavigate();
+
+    const {id} = useParams();
+
+    useEffect(()=>{
+        const fetchNote = async () => {
+            try {
+                const res = await api.get(`/notes/${id}`)
+                setNote(res.data);
+            } catch (error) {
+                console.log("Error in fetching note",error);
+                toast.error("Failed to fetch the note");
+            } finally{
+                setLoading(false);
+            }
+        }
+        fetchNote();
+    },[id])
+
+    console.log({note})
+
     return(
         <div>NoteDetailPage</div>
     )
